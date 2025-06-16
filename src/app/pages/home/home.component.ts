f1272f8ef484c00c2a689de85952bd60f0641348
lucas <ls.marinho36@gmail.com>
2025-06-16 18:05:37 -0300

    feat(big-card): create html estructure and css style from card main

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { BigCardComponent } from '../../components/big-card/big-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, MenuTitleComponent],
+  imports: [HeaderComponent, MenuTitleComponent, BigCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
