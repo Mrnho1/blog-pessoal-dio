63055a6f9ac8f867cffadf07d299271c0504e35c
lucas <ls.marinho36@gmail.com>
2025-06-16 12:20:26 -0300

    feat(header):adding navigation bar to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
-import { BigCardComponent } from '../../components/big-card/big-card.component';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent, BigCardComponent],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
