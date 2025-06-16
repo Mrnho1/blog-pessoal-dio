4562dd59ea2bf3576f9cbf52bbcde31c20c7d5c0
lucas <ls.marinho36@gmail.com>
2025-06-16 11:08:03 -0300

    fate: add splash animation screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'meu blog pessoal';
+  isLoading = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2200);
+  }
 }
