57b6b001b77c4d19f2f2eb4d36721b6699467517
lucas <ls.marinho36@gmail.com>
2025-06-16 12:45:42 -0300

    feat(menu-title): add main page title

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MenuTitleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
